49f1f9f8d076ee96a2dad34d61609d69697695d5
CharlesJ9752 <1499695378@qq.com>
2022-09-12 01:30:57 +0800

    fix name

---
+++ cdp_ede_local-Lab04_exp7/mycpu_env/myCPU/ID.v
@@ -0,0 +1,10 @@
+//译码，生成操作数，写回寄存器堆
+module  ID (
+    input           clk,
+    input           resetn,
+    //与IF阶段
+    input           if_id_valid,
+    output          id_if_allowin,
+
+);
+endmodule
