90c597ac8316f898f29ceb7116d597826e568c52
CharlesJ9752 <1499695378@qq.com>
2022-09-30 17:31:38 +0800

    exp9

--- Lab05_exp10/mycpu_env/myCPU/EXE.v
+++ Lab05_exp10/mycpu_env/myCPU/EXE.v
@@ -1,11 +1,11 @@
-//运行alu，写存储器
+//运行alu，写存储�?
 module EXE (
     input           clk,
     input           resetn,
     //与ID阶段
     output          exe_allowin,
     input           id_exe_valid,
-    input   [179:0] id_exe_bus,
+    input   [186:0] id_exe_bus,
     //与MEM阶段
     output          exe_mem_valid,
     input           mem_allowin,
@@ -15,7 +15,7 @@ module EXE (
     output  [ 3:0]  data_sram_we,
     output  [31:0]  data_sram_addr,
     output  [31:0]  data_sram_wdata,
-    //写信号
+    //写信�?
     output  [ 38:0]  exe_wr_bus
 );
     //信号定义
@@ -23,10 +23,13 @@ module EXE (
     wire            exe_ready_go;
     wire    [ 31:0] exe_inst;
     wire    [ 31:0] exe_pc;
-    reg     [179:0] id_exe_bus_vld;
+    reg     [186:0] id_exe_bus_vld;
     wire            exe_en_bypass;
     wire            exe_en_block;
-    assign  exe_ready_go = 1'b1;
+    assign exe_ready_go    = 
+    (alu_op[15] || alu_op[17])     ? signed_dout_tvalid :
+    (alu_op[16] || alu_op[18])    ? unsigned_dout_tvalid :
+    1'b1;
     assign  exe_mem_valid = exe_ready_go & exe_valid;
     assign  exe_allowin = exe_mem_valid & mem_allowin | ~exe_valid;
     always @(posedge clk ) begin
@@ -46,7 +49,7 @@ module EXE (
     wire            exe_gr_we;
     wire            exe_mem_we;
     wire            exe_res_from_mem;
-    wire    [11:0]  alu_op;
+    wire    [18:0]  alu_op;
     wire    [31:0]  alu_src1;
     wire    [31:0]  alu_src2;
     wire    [ 4:0]  exe_dest;
@@ -58,6 +61,7 @@ module EXE (
     } = id_exe_bus_vld;
     //运行alu
     wire    [31:0]  alu_result;
+    wire    [31:0]  exe_result;
     alu my_alu (
         .alu_op(alu_op),
         .alu_src1(alu_src1),
@@ -71,12 +75,116 @@ module EXE (
     assign  data_sram_wdata = exe_rkd_value;
     assign  exe_mem_bus = {
         exe_gr_we, exe_res_from_mem, exe_dest,
-        exe_pc, exe_inst, alu_result
+        exe_pc, exe_inst, exe_result
     };
-    //写信号
+    //写信�?
     assign  exe_en_bypass = exe_valid & exe_gr_we;
     assign  exe_en_block = exe_valid & exe_res_from_mem;//in case of load
     assign exe_wr_bus = {
-        exe_en_bypass, exe_en_block, exe_dest, alu_result
+        exe_en_bypass, exe_en_block, exe_dest, exe_result
     };
+    
+//���ó���IP
+assign exe_result=(alu_op[15])? signed_divider_res[63:32]:   
+                   (alu_op[16])? unsigned_divider_res[63:32]:
+                   (alu_op[17])? signed_divider_res[31:0]:   
+                   (alu_op[18])? unsigned_divider_res[31:0]:alu_result;
+                   
+                   
+wire [31:0] divider_dividend;
+wire [31:0] divider_divisor;
+wire [63:0] unsigned_divider_res;
+wire [63:0] signed_divider_res;
+assign divider_dividend = alu_src1;
+assign divider_divisor  = alu_src2;
+
+wire unsigned_dividend_tready;
+wire unsigned_dividend_tvalid;
+wire unsigned_divisor_tready;
+wire unsigned_divisor_tvalid;
+wire unsigned_dout_tvalid;
+
+wire signed_dividend_tready;
+wire signed_dividend_tvalid;
+wire signed_divisor_tready;
+wire signed_divisor_tvalid;
+wire signed_dout_tvalid;
+
+my_divu u_unsigned_divider (
+    .aclk                   (clk),
+    .s_axis_dividend_tdata  (divider_dividend),
+    .s_axis_dividend_tready (unsigned_dividend_tready),
+    .s_axis_dividend_tvalid (unsigned_dividend_tvalid),
+    .s_axis_divisor_tdata   (divider_divisor),
+    .s_axis_divisor_tready  (unsigned_divisor_tready),
+    .s_axis_divisor_tvalid  (unsigned_divisor_tvalid),
+    .m_axis_dout_tdata      (unsigned_divider_res),
+    .m_axis_dout_tvalid     (unsigned_dout_tvalid)
+);
+
+my_div u_signed_divider (
+    .aclk                   (clk),
+    .s_axis_dividend_tdata  (divider_dividend),
+    .s_axis_dividend_tready (signed_dividend_tready),
+    .s_axis_dividend_tvalid (signed_dividend_tvalid),
+    .s_axis_divisor_tdata   (divider_divisor),
+    .s_axis_divisor_tready  (signed_divisor_tready),
+    .s_axis_divisor_tvalid  (signed_divisor_tvalid),
+    .m_axis_dout_tdata      (signed_divider_res),
+    .m_axis_dout_tvalid     (signed_dout_tvalid)
+);
+    
+reg  unsigned_dividend_sent;
+reg  unsigned_divisor_sent;
+
+assign unsigned_dividend_tvalid = exe_valid && (alu_op[16] || alu_op[18]) && !unsigned_dividend_sent;
+assign unsigned_divisor_tvalid = exe_valid && (alu_op[16] || alu_op[18]) && !unsigned_divisor_sent;
+
+always @ (posedge clk) begin
+    if (!resetn) begin
+        unsigned_dividend_sent <= 1'b0;
+    end else if (unsigned_dividend_tready && unsigned_dividend_tvalid) begin
+        unsigned_dividend_sent <= 1'b1;
+    end else if (exe_ready_go && mem_allowin) begin
+        unsigned_dividend_sent <= 1'b0;
+    end
+    
+    if (!resetn) begin
+        unsigned_divisor_sent <= 1'b0;
+    end else if (unsigned_divisor_tready && unsigned_divisor_tvalid) begin
+        unsigned_divisor_sent <= 1'b1;
+    end else if (exe_ready_go && mem_allowin) begin
+        unsigned_divisor_sent <= 1'b0;
+    end
+end
+
+reg  signed_dividend_sent;
+reg  signed_divisor_sent;
+
+assign signed_dividend_tvalid = exe_valid && (alu_op[15] || alu_op[17]) && !signed_dividend_sent;
+assign signed_divisor_tvalid = exe_valid && (alu_op[15] || alu_op[17]) && !signed_divisor_sent;
+
+always @ (posedge clk) begin
+    if (!resetn) begin
+        signed_dividend_sent <= 1'b0;
+    end else if (signed_dividend_tready && signed_dividend_tvalid) begin
+        signed_dividend_sent <= 1'b1;
+    end else if (exe_ready_go && mem_allowin) begin
+        signed_dividend_sent <= 1'b0;
+    end
+    
+    if (!resetn) begin
+        signed_divisor_sent <= 1'b0;
+    end else if (signed_divisor_tready && signed_divisor_tvalid) begin
+        signed_divisor_sent <= 1'b1;
+    end else if (exe_ready_go && mem_allowin) begin
+        signed_divisor_sent <= 1'b0;
+    end
+
+end
+
+    
+    
+    
+    
 endmodule
--- Lab05_exp10/mycpu_env/myCPU/ID.v
+++ Lab05_exp10/mycpu_env/myCPU/ID.v
@@ -10,7 +10,7 @@ module ID (
     //与EXE阶段
     input           exe_allowin,
     output          id_exe_valid,
-    output  [179:0] id_exe_bus,
+    output  [186:0] id_exe_bus,
     //来自WB阶段
     input   [ 37:0] wb_id_bus,
     //来自各级的写使能和写地址信号，用于判断阻�?
@@ -45,9 +45,15 @@ module ID (
     } = mem_wr_bus;
     assign addr1_valid = inst_add_w | inst_sub_w | inst_slt | inst_addi_w | inst_sltu | 
                             inst_nor | inst_and | inst_or | inst_xor | inst_srli_w | 
-                            inst_slli_w | inst_srai_w | inst_ld_w | inst_st_w |inst_bne  | inst_beq | inst_jirl;
+                            inst_slli_w | inst_srai_w | inst_ld_w | inst_st_w |inst_bne  | inst_beq | inst_jirl |
+                            inst_slti | inst_sltui | inst_andi | inst_ori | inst_xori |
+                            inst_sll_w | inst_srl_w | inst_sra_w | 
+                            inst_mul_w | inst_mulh_w | inst_mulh_wu | inst_div_w | inst_div_wu | inst_mod_w | inst_mod_wu;
     assign addr2_valid = inst_add_w | inst_sub_w | inst_slt | inst_sltu | inst_and | 
-                         inst_or | inst_nor | inst_xor | inst_st_w | inst_beq | inst_bne;
+                         inst_or | inst_nor | inst_xor | inst_st_w | inst_beq | inst_bne |
+                         inst_sll_w | inst_srl_w | inst_sra_w |
+                         inst_sll_w | inst_srl_w | inst_sra_w | 
+                         inst_mul_w | inst_mulh_w | inst_mulh_wu | inst_div_w | inst_div_wu | inst_mod_w | inst_mod_wu;
 
     assign id_ready_go = ~( exe_en_block & ((exe_dest==rf_raddr1) & addr1_valid
                                             |(exe_dest==rf_raddr2) & addr2_valid));//in case of load
@@ -98,10 +104,19 @@ module ID (
     wire        inst_and;
     wire        inst_or;
     wire        inst_xor;
+    wire        inst_slti;
+    wire        inst_sltui;
+    wire        inst_andi;
+    wire        inst_ori;
+    wire        inst_xori;
+    wire        inst_sll_w;
+    wire        inst_srl_w;
+    wire        inst_sra_w;
     wire        inst_slli_w;
     wire        inst_srli_w;
     wire        inst_srai_w;
     wire        inst_addi_w;
+    wire        inst_pcaddu12i;
     wire        inst_ld_w;
     wire        inst_st_w;
     wire        inst_jirl;
@@ -110,6 +125,9 @@ module ID (
     wire        inst_beq;
     wire        inst_bne;
     wire        inst_lu12i_w;
+    wire        inst_mul_w;
+    wire        inst_mulh_w;
+    wire        inst_mulh_wu;
 
     wire        need_ui5;
     wire        need_si12;
@@ -125,7 +143,7 @@ module ID (
     wire        rf_we   ;
     wire [ 4:0] rf_waddr;
     wire [31:0] rf_wdata;
-    wire [11:0] id_alu_op;
+    wire [18:0] id_alu_op;
     wire [31:0] id_alu_src1   ;
     wire [31:0] id_alu_src2   ;
     wire [31:0] alu_result ;
@@ -178,31 +196,56 @@ module ID (
     assign inst_beq    = op_31_26_d[6'h16];
     assign inst_bne    = op_31_26_d[6'h17];
     assign inst_lu12i_w= op_31_26_d[6'h05] & ~id_inst[25];
+    assign inst_slti   = op_31_26_d[6'h00] & op_25_22_d[4'h8];
+    assign inst_sltui  = op_31_26_d[6'h00] & op_25_22_d[4'h9];
+    assign inst_andi   = op_31_26_d[6'h00] & op_25_22_d[4'hd];
+    assign inst_ori    = op_31_26_d[6'h00] & op_25_22_d[4'he];
+    assign inst_xori   = op_31_26_d[6'h00] & op_25_22_d[4'hf];
+    assign inst_sll_w  = op_31_26_d[6'h00] & op_25_22_d[4'h0] & op_21_20_d[2'h1] & op_19_15_d[5'h0e];
+    assign inst_srl_w  = op_31_26_d[6'h00] & op_25_22_d[4'h0] & op_21_20_d[2'h1] & op_19_15_d[5'h0f];
+    assign inst_sra_w  = op_31_26_d[6'h00] & op_25_22_d[4'h0] & op_21_20_d[2'h1] & op_19_15_d[5'h10];
+    assign inst_pcaddu12i = op_31_26_d[6'h07] & ~id_inst[25];
+    assign inst_mul_w  = op_31_26_d[6'h0] & op_25_22_d[4'h0] & op_21_20_d[2'h1] & op_19_15_d[5'h18];
+    assign inst_mulh_w = op_31_26_d[6'h0] & op_25_22_d[4'h0] & op_21_20_d[2'h1] & op_19_15_d[5'h19];
+    assign inst_mulh_wu= op_31_26_d[6'h0] & op_25_22_d[4'h0] & op_21_20_d[2'h1] & op_19_15_d[5'h1a];
+    assign inst_div_w  = op_31_26_d[6'h0] & op_25_22_d[4'h0] & op_21_20_d[2'h2] & op_19_15_d[5'h00];
+    assign inst_mod_w  = op_31_26_d[6'h0] & op_25_22_d[4'h0] & op_21_20_d[2'h2] & op_19_15_d[5'h01];
+    assign inst_div_wu = op_31_26_d[6'h0] & op_25_22_d[4'h0] & op_21_20_d[2'h2] & op_19_15_d[5'h02];
+    assign inst_mod_wu = op_31_26_d[6'h0] & op_25_22_d[4'h0] & op_21_20_d[2'h2] & op_19_15_d[5'h03];
 
     assign id_alu_op[ 0] = inst_add_w | inst_addi_w | inst_ld_w | inst_st_w
-                        | inst_jirl | inst_bl;
+                        | inst_jirl | inst_bl | inst_pcaddu12i;
     assign id_alu_op[ 1] = inst_sub_w;
-    assign id_alu_op[ 2] = inst_slt;
-    assign id_alu_op[ 3] = inst_sltu;
-    assign id_alu_op[ 4] = inst_and;
+    assign id_alu_op[ 2] = inst_slt | inst_slti;
+    assign id_alu_op[ 3] = inst_sltu | inst_sltui;
+    assign id_alu_op[ 4] = inst_and | inst_andi;
     assign id_alu_op[ 5] = inst_nor;
-    assign id_alu_op[ 6] = inst_or;
-    assign id_alu_op[ 7] = inst_xor;
-    assign id_alu_op[ 8] = inst_slli_w;
-    assign id_alu_op[ 9] = inst_srli_w;
-    assign id_alu_op[10] = inst_srai_w;
+    assign id_alu_op[ 6] = inst_or | inst_ori;
+    assign id_alu_op[ 7] = inst_xor | inst_xori;
+    assign id_alu_op[ 8] = inst_slli_w | inst_sll_w;
+    assign id_alu_op[ 9] = inst_srli_w | inst_srl_w;
+    assign id_alu_op[10] = inst_srai_w | inst_sra_w;
     assign id_alu_op[11] = inst_lu12i_w;
+    assign id_alu_op[12] = inst_mul_w;
+    assign id_alu_op[13] = inst_mulh_w;
+    assign id_alu_op[14] = inst_mulh_wu;
+    assign id_alu_op[15] = inst_div_w;
+    assign id_alu_op[16] = inst_div_wu;
+    assign id_alu_op[17] = inst_mod_w;
+    assign id_alu_op[18] = inst_mod_wu;
 
     assign need_ui5   =  inst_slli_w | inst_srli_w | inst_srai_w;
     assign need_si12  =  inst_addi_w | inst_ld_w | inst_st_w;
     assign need_si16  =  inst_jirl | inst_beq | inst_bne;
-    assign need_si20  =  inst_lu12i_w;
+    assign need_si20  =  inst_lu12i_w | inst_pcaddu12i;
     assign need_si26  =  inst_b | inst_bl;
+    assign need_zero_xtnd = inst_ori | inst_andi | inst_xori;
     assign src2_is_4  =  inst_jirl | inst_bl;
 
     assign imm = src2_is_4 ? 32'h4                      :
-                need_si20 ? {i20[19:0], 12'b0}         :
-    /*need_ui5 || need_si12*/{{20{i12[11]}}, i12[11:0]} ;
+                need_si20 ? {i20[19:0], 12'b0}          :
+                need_zero_xtnd ? {20'b0, i12[11:0]}     :
+                                {{20{i12[11]}}, i12[11:0]} ;
 
     assign br_offs = need_si26 ? {{ 4{i26[25]}}, i26[25:0], 2'b0} :
                                 {{14{i16[15]}}, i16[15:0], 2'b0} ;
@@ -211,7 +254,7 @@ module ID (
 
     assign src_reg_is_rd = inst_beq | inst_bne | inst_st_w;
 
-    assign src1_is_pc    = inst_jirl | inst_bl;
+    assign src1_is_pc    = inst_jirl | inst_bl | inst_pcaddu12i;
 
     assign src2_is_imm   = inst_slli_w |
                         inst_srli_w |
@@ -221,7 +264,13 @@ module ID (
                         inst_st_w   |
                         inst_lu12i_w|
                         inst_jirl   |
-                        inst_bl     ;
+                        inst_bl     |
+                        inst_slti   |
+                        inst_sltui  |
+                        inst_andi   |
+                        inst_ori    |
+                        inst_xori   |
+                        inst_pcaddu12i;
 
     assign id_res_from_mem  = inst_ld_w;
     assign dst_is_r1     = inst_bl;
